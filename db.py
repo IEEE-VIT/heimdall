8ed34b95589b512e41ee8934c27cc27315e7ae68
Alok <alokme123@gmail.com>
2021-07-08 19:39:17 +0530

    feat: add SQLite3 Support

--- db.py
+++ db.py
@@ -26,6 +26,48 @@ class Database:
         elif dbType == "SQLITE3":
             return self.write_sqlite(data,delete)
 
+    def createSQLiteDB(self):
+        con = sqlite3.connect('invites.db')
+        cur = con.cursor()
+        cur.execute('CREATE TABLE invites(invite_code text, uses int, role_linked text, role_id text)')
+        con.commit()
+        con.close()
+        print("Created Invites DB!")
+
+    def fetch_sqlite(self):
+        try:
+            if(not(path.exists("invites.db"))):
+                return {}
+            con = sqlite3.connect('invites.db')
+            cur = con.cursor()
+            result = cur.execute('SELECT * FROM invites').fetchall()
+            data = []
+            for row in result:
+                data.append({"invite_code": row[0], "uses": row[1],"role_linked": row[2], "role_id": row[3]})
+            con.commit()
+            con.close()
+            return data
+        except:
+            print("Error while fetching from SQLite3 DB")
+            return {}
+
+    def write_sqlite(self, data, delete):
+        try:
+            if(not(path.exists("invites.db"))):
+                self.createSQLiteDB()
+            con = sqlite3.connect('invites.db')
+            cur = con.cursor()
+            if(delete):
+                cur.execute(f'DELETE FROM invites WHERE invite_code="{data["invite_code"]}"')
+            else:
+                cur.execute(f'INSERT INTO invites(invite_code, uses, role_linked, role_id) VALUES("{data["invite_code"]}", {data["uses"]}, "{data["role_linked"]}", "{data["role_id"]}")')
+            con.commit()
+            con.close()
+            return True
+        except:
+            print("Error while writing to SQLite3 DB")
+            return False
+
     def fetch_json(self):
         try:
             with open('data.json') as f:
